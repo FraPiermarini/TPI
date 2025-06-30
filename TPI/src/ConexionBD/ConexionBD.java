6ea627216c304723d99d7a335e9f7f2b494d49c7
Marcinovisardi05 <marcionovisardi00@gmail.com>
2025-06-30 18:21:31 -0300

    Update ConexionBD.java

--- TPI/src/ConexionBD/ConexionBD.java
+++ TPI/src/ConexionBD/ConexionBD.java
@@ -1,5 +1,23 @@
-package ConexionBD;
+package conexion;
 
-public class ConexionBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class conexionBD {
+   private static final String URL = "jdbc:mysql://localhost:3306/vete";
+   private static final String USER = "root";
+   private static final String PASSWORD = "tpi123";
+
+   public conexionBD() {
+   }
+
+   public static Connection getConnection() throws SQLException {
+      try {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         return DriverManager.getConnection("jdbc:mysql://localhost:3306/vete", "root", "Marcio33");
+      } catch (ClassNotFoundException var1) {
+         throw new SQLException("MySQL JDBC Driver no encontrado", var1);
+      }
+   }
 }
